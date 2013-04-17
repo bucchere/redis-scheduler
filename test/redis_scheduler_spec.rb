06f6f3aedb386e9748f6385fec05561cdd187995
Chris Bucchere <chris@dijit.com>
2013-04-17 10:34:13 -0700

    learned how to count

--- test/redis_scheduler_spec.rb
+++ test/redis_scheduler_spec.rb
@@ -12,7 +12,8 @@ describe RedisScheduler do
     @id1 = @scheduler.schedule!("testing1", Time.now.to_i, 1)
     @id2 = @scheduler.schedule!("testing2", Time.now.to_i, 1)
     @id3 = @scheduler.schedule!(URI::encode({ "testing" => 3 }.to_json), Time.now.to_i, 2)
-    @scheduler.size.should == 3
+    @id_future = @scheduler.schedule!("future", (Time.now + 100000).to_i, 3)
+    @scheduler.size.should == 4
   end
 
   it "should schedule an item" do
@@ -22,17 +23,17 @@ describe RedisScheduler do
 
   it "should allow unscheudling by user" do
     @scheduler.unschedule_for!(1).should == [{ @id1.to_s => 'testing1' }, { @id2.to_s => 'testing2' }]
-    @scheduler.size.should == 1
+    @scheduler.size.should == 2
   end
 
   it "should return an array of job_id => payload hashes for a given user" do
     @scheduler.scheduled_for(1).should == [{ @id1.to_s => 'testing1' }, { @id2.to_s => 'testing2' }]
-    @scheduler.size.should == 3
+    @scheduler.size.should == 4
   end
 
   it "should unschedule, remove and return a given job for a given user" do
     @scheduler.unschedule!(1, @id1).should == { @id1.to_s => 'testing1' }
-    @scheduler.size.should == 2
+    @scheduler.size.should == 3
   end
 
   it "should iterate over items ready to be executed" do
