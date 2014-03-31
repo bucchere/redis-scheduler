d68b85ec8a0e113f6271e0f51bbd024d2c4e3f6c
Chris Bucchere <chris@dijit.com>
2014-03-31 16:10:30 -0700

    test cleanup and added a few more cases

--- test/redis_scheduler_spec.rb
+++ test/redis_scheduler_spec.rb
@@ -1,5 +1,6 @@
 require 'redis'
 require 'json'
+require 'timecop'
 require './lib/redis-scheduler.rb'
 
 describe RedisScheduler do
@@ -10,40 +11,59 @@ describe RedisScheduler do
     @scheduler = RedisScheduler.new(@redis, :namespace => 'testing')
     @scheduler.reset!
     @scheduler.should_not == nil
-    @id1 = @scheduler.schedule!("testing1", @now, 1)
-    @id2 = @scheduler.schedule!("testing2", @now, 1)
-    @id3 = @scheduler.schedule!(URI::encode({ "testing" => 3 }.to_json), @now, 2)
-    @id_future = @scheduler.schedule!("future", @now + 100000, 3)
+    @id1 = @scheduler.schedule!('testing1', @now, 1)
+    @id2 = @scheduler.schedule!('testing2', @now, 1)
+    @id3 = @scheduler.schedule!('testing3', @now, 2)
+    @id_future = @scheduler.schedule!('future', @now + 100000, 3)
+    @scheduler.size.should == 4
+    @id = nil
+  end
+
+  it 'should schedule an item' do
+    @id = @scheduler.schedule!('testing', @now, 3)
+    @scheduler.item(@id).should == { @id => 'testing' }
+    @scheduler.size.should == 5
+  end
+
+  it 'should unschedule an item' do
+    @id = @scheduler.schedule!('testing', @now.to_f, 3)
+    @scheduler.item(@id).should == { @id => 'testing' }
+    @scheduler.size.should == 5
+    payload = @scheduler.unschedule!(3, [@id])
+    payload[0][@id][0].should == 'testing'
     @scheduler.size.should == 4
   end
-
-  it "should schedule an item" do
-    @id = @scheduler.schedule!("testing", @now)
-    @scheduler.item(@id).should == { @id => "testing" }
+  
+  it 'should allow unscheudling by user' do
+    jobs = @scheduler.unschedule_all_for!(1)
+    jobs[0][@id1][0].should == 'testing1'
+    jobs[1][@id2][0].should ==  'testing2'
+    @scheduler.size.should == 2 #only two jobs remain on the schedule
+    @scheduler.scheduled_for(1).should == [] #no jobs for user 1
   end
 
-  it "should allow unscheudling by user" do
-    @scheduler.unschedule_all_for!(1).should == [{ @id1 => ['testing1', @now] }, { @id2 => ['testing2', @now] }]
-    @scheduler.size.should == 2
-    @scheduler.scheduled_for(1).should == []
+  it 'should return an array of job_id => payload hashes for a given user' do
+    user1_jobs = @scheduler.scheduled_for(1)
+    user1_jobs[0][@id1][0] = 'testing1' 
+    user1_jobs[0][@id1][0] = 'testing2' 
+    @scheduler.size.should == 4 #no changes to schedule
   end
 
-  it "should return an array of job_id => payload hashes for a given user" do
-    @scheduler.scheduled_for(1).should == [{ @id1 => ['testing1', @now] }, { @id2  => ['testing2', @now] }]
-    @scheduler.size.should == 4
-  end
-
-  it "should unschedule, remove and return a given job for a given user" do
-    @scheduler.unschedule!(1, [@id1]).should == [2]
+  it 'should unschedule, remove and return a given job for a given user' do
+    user1_jobs = @scheduler.unschedule!(1, [@id1])
+    user1_jobs[0][@id1][0] = 'testing1'    
     @scheduler.size.should == 3
   end
 
-  it "should iterate over items ready to be executed" do
-    @scheduler.each do |entry, time, job_id|
-      if entry != 'testing1' and entry != 'testing2'
-        JSON::parse(URI::decode(entry))["testing"].should == 3
+  it 'should iterate over items ready to be executed' do
+    begin
+      Timecop.travel Time.now + 5000
+      @scheduler.each do |entry, time, job_id|
+        #this space intentionally left blank
       end
+      @scheduler.size.should == 1
+    ensure 
+      Timecop.return
     end
-    @scheduler.size.should == 1
   end
 end
